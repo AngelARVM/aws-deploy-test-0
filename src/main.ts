a4d1e9fe3433f14e0f64591eca633126019100fa
AngelARVM <angelarvm@gmail.com>
2023-06-01 16:35:07 -0300

    devops: add task definition json, add new steps to aws deploy action

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT;
+  const environment = process.env.NODE_ENV || 'development';
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app
+    .listen(port)
+    .then(() =>
+      environment
+        ? console.log('Server is running at url http://localhost:' + port)
+        : null,
+    );
 }
 bootstrap();
